c34b771c82d566f135a12e97657e214a891eba22
ken <block24block@gmail.com>
2019-06-29 16:19:46 +0000

    can not look up current dir

--- rdev.sh
+++ rdev.sh
@@ -1,6 +1,10 @@
-docker build -t $2 -<<EOF
+cat <<EOF > Dockerfile.rdev
 FROM daocloud.io/shutdown/rdev:$1
 
 COPY Gemfile Gemfile.lock ./
 RUN gem install bundler && bundle install --jobs 20 --retry 5
 EOF
+
+docker build -t $2 -f Dockerfile.rdev .
+
+rm Dockerfile.rdev
