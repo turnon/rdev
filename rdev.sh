936849bddb6f4ed24a24cf4a3858e986442a59ec
ken <block24block@gmail.com>
2019-06-30 08:01:10 +0000

    default cmd

--- rdev.sh
+++ rdev.sh
@@ -3,6 +3,8 @@ FROM daocloud.io/shutdown/rdev:$1
 
 COPY Gemfile Gemfile.lock ./
 RUN gem install bundler && bundle install --jobs 20 --retry 5
+
+CMD ["/bin/bash"]
 EOF
 
 docker build -t $2 -f Dockerfile.rdev .
